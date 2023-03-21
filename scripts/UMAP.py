5fc9c6e80994441f76c09dc00c5efc3ff06a3e4e
Nishant Joshi <mr.nishantjoshi@gmail.com>
2023-03-21 09:44:44 +0100

    removed non-essential parts

--- scripts/UMAP.py
+++ scripts/UMAP.py
@@ -1,4 +1,3 @@
-#%%
 import sklearn.cluster as cluster
 from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 from sklearn import manifold
@@ -32,7 +31,7 @@ import pickle
 from PCA import * 
 # umap.UMAP()
 import umap.umap_ as umap
-#%%
+
 def plot_UMAP(data_inh,data_exc,c_exc,c_inh,neighbours,distance,legend):
     """plots UMAP for excitatory and inhibitory cells 
 
@@ -87,57 +86,3 @@ def plot_UMAP(data_inh,data_exc,c_exc,c_inh,neighbours,distance,legend):
 
     plt.show()
 
-#%%
-# data_d2 = pickle.load(open('G:/My Drive/all_ephys_d2.p','rb'))
-# data_d1 = pickle.load(open('G:/My Drive/all_ephys_d1.p','rb'))
-# data_sag = pickle.load(open('G:/My Drive/all_ephys_sag.p','rb'))
-# data_acsf_exc = pickle.load(open('G:/My Drive/all_ephys_exc_NC_acsf_imp.p','rb'))
-# data_acsf_inh = pickle.load(open('G:/My Drive/all_ephys_inh_NC_acsf_imp.p','rb'))
-
-# ind_feat = [0,3,4,6,9,17,20]
-
-# # perplexity_ = np.arange(5,100,10)
-# # fig, ax_inh = plt.subplots(1,len(perplexity_),figsize=[40,4])
-# # fig, ax_exc = plt.subplots(1,len(perplexity_),figsize=[40,4])
-
-
-
-# data_all_inh = np.vstack([  remove_nans_and_infs(np.squeeze(np.array(data_d2['inh'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['inh'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_sag['inh'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf'])))[:,ind_feat]])
-
-# data_all_exc = np.vstack([  remove_nans_and_infs(np.squeeze(np.array(data_d2['exc'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['exc'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_sag['exc'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf'])))[:,ind_feat],
-#                             remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf'])))[:,ind_feat]])
-
-# colors_inh = [np.repeat('red',len(remove_nans_and_infs(np.squeeze(np.array(data_d2['inh']))))),
-# np.repeat('green',len(remove_nans_and_infs(np.squeeze(np.array(data_sag['inh']))))),
-# np.repeat('blue',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['inh']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['inh_acsf'])))))]
-
-# colors_exc = [np.repeat('red',len(remove_nans_and_infs(np.squeeze(np.array(data_d2['exc']))))),
-# np.repeat('green',len(remove_nans_and_infs(np.squeeze(np.array(data_sag['exc']))))),
-# np.repeat('blue',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['exc']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf']))))),
-# np.repeat('orange',len(remove_nans_and_infs(np.squeeze(np.array(data_d1['exc_acsf'])))))]
-# neighbours  = np.arange(5,50,5)
-# distances = np.arange(0.05,0.5,0.05)
-# for n in neighbours:
-#     for dist in distances:
-#         print(n, dist)
-#         plot_UMAP(data_acsf_inh['all'][:,ind_feat],data_acsf_exc['all'][:,ind_feat],'blue','blue',n,dist,['acsf'])
-#         # plot_UMAP(data_all_inh,data_all_exc,c_inh=np.hstack(colors_inh),c_exc=np.hstack(colors_exc),neighbours=n,distance=dist,legend=['sag','D1','D2','acsf'])
-# plot_UMAP(data_all_inh,data_all_exc,c_inh=np.hstack(colors_inh),c_exc=np.hstack(colors_exc))
-# plt.legend(['sag','D1','D2','acsf'])
-# plt.show() 
-    
-# %%
